7fa0111413f2faf0530dd745893fa987d4650b94
NikhilRao29 <153190657+NikhilRao29@users.noreply.github.com>
2024-01-02 03:52:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const element = document.querySelectorAll('#elem')
+const image = document.querySelector('img')
+
+
+// element.addEventListener('mousemove', function(dets){
+//   image.style.left = dets.x + "px"
+//   image.style.top = dets.y + "px"
+// })
+
+
+// element.addEventListener('mouseenter', function(){
+//   image.style.opacity = 1
+// })
+
+// element.addEventListener('mouseleave', function(){
+//   image.style.opacity = 0
+// })
+
+
+element.forEach(function(val){
+  console.log(val.childNodes)
+  
+  val.addEventListener('mouseenter', function(){
+    val.childNodes[3].style.opacity = 1
+    val.childNodes[1].style.opacity = 0.3
+  })
+
+  val.addEventListener('mouseleave', function(){
+    val.childNodes[3].style.opacity = 0
+    val.childNodes[1].style.opacity = 1
+  })
+
+  val.addEventListener('mousemove', function(dets){
+    val.childNodes[3].style.left = dets.x + "px"
+    // val.childNodes[3].style.top = dets.y + "px"
+  })
+
+})
